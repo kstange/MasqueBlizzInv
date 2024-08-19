f636d7bd20724ff12024d38dac2932b8d32e0f03
Kevin Stange <kstange@gmail.com>
2024-08-18 19:03:34 -0500

    Avoid use of C_AddOns.GetAddOnMetadata

--- Metadata.lua
+++ Metadata.lua
@@ -21,9 +21,6 @@ Shared.Metadata = Metadata
 Metadata.FriendlyName = "Masque Blizzard Inventory"
 Metadata.MasqueFriendlyName = "Blizzard Inventory"
 
--- Compatibility for WoW 10.1.0
-local GetAddOnMetadata = _G.GetAddOnMetadata or C_AddOns.GetAddOnMetadata
-
 -- Title will be used for the group name shown in Masque
 -- Delayed indicates this group will be deferred to a hook or event
 -- Init is a function that will be run at load time for this group
@@ -397,8 +394,7 @@ Metadata.OptionCallbacks = {}
 -- AceConfig Options table used to display a panel.
 Metadata.Options = {
 	type = "group",
-	name = format(L["OPTIONS_TITLE_MAIN"], Metadata.FriendlyName) .. "     |cFFAAAAAA" ..
-	              (GetAddOnMetadata(AddonName, "Version") or "Unknown"),
+	name = format(L["OPTIONS_TITLE_MAIN"], Metadata.FriendlyName) .. "     |cFFAAAAAA@project-version@",
 	args = {
 		Description = {
 			name = L["OPTIONS_DESCRIPTION_MAIN"] .. "\n ",
